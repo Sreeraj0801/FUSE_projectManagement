f2d73736b8bdb65f90a973f4b58b22f22fb1709c
Sreeraj R <sreeraj.r.0801@gmail.com>
2023-05-15 11:08:45 +0530

    socket.io server

--- frontend/dist/assets/UserProfile-23569f16.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-4392a1d0.js";const a=()=>r("div",{className:"dark:bg-slate-800  bg-gray-300  lg:p-8 p-3 h-screen overflow-y-auto "});export{a as default};
---
+++ frontend/dist/assets/UserProfile-71aa835e.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-bd65f0c3.js";const a=()=>r("div",{className:"dark:bg-slate-800  bg-gray-300  lg:p-8 p-3 h-screen overflow-y-auto "});export{a as default};
